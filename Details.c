314fc745eed80bf4199f76a63c3a4c5330e13cca
samialjabirmahin <111931371+samialjabirmahin@users.noreply.github.com>
2025-05-10 01:13:27 +0600

    Add files via upload

---
+++ Details.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   
+    printf("Name: Sami Al Jabir Mahin\n");
+    printf("DOB: 22 October 2004\n");
+    printf("Mobile Number: 01732613742");
+
+    return 0;
+}
